7dd23755b4d2b6a80a6ff707198ec75ff82feefd
Barn Sweetman <barnsweetman@gmail.com>
2015-07-27 16:28:22 -0500

    Getting authentication working and getting results from PC

--- sites/all/modules/_custom/ew_pc_integration/pc-php-library/oauth_config.php
+++ sites/all/modules/_custom/ew_pc_integration/pc-php-library/oauth_config.php
@@ -1,6 +1,6 @@
 <?php
-define("CONSUMER_KEY", "");
-define("CONSUMER_SECRET", "");
+define("CONSUMER_KEY", "8vpSpyf23GBw50nnA1W8");
+define("CONSUMER_SECRET", "9xRs5MbXT0MyPNoWG8ZdB15BXeWB9uRaPtCe8LYg");
 $callback_url = "http".(isset($_SERVER['HTTPS']) ? 's' : '').'://'.$_SERVER['HTTP_HOST']."/pc-api-callback";
 define("CALLBACK_URL", $callback_url);
 
