d1b6be0375a21bfb703a64c47c38736237c2bb8b
Evan Willhite <ewillhite@centresource.com>
2015-09-10 10:15:35 -0500

    password_policy, mobile_codes and ga_login modules as well as tweaks to go along

--- sites/all/themes/ewo/assets/js/application.js
+++ sites/all/themes/ewo/assets/js/application.js
@@ -15,6 +15,7 @@
     });
     $('.not-logged-in header nav .login a').click(function(e) {
       $('#myModal').modal('show');
+      $('#myModal .modal-body .password-suggestions').prependTo('#myModal .modal-body .confirm-parent');
       return e.preventDefault();
     });
     $('#better-messages-default .close').click(function() {
